7287ec56d67017c4b5c94ab6c899c8b373858cbf
fengqi <lyf362345@gmail.com>
2024-01-14 22:09:33 +0800

    代码清理

--- cache/cache.go
+++ cache/cache.go
@@ -19,20 +19,3 @@ func InitCache(path string) {
 		}
 	}()
 }
-
-type Store interface {
-	Close() error
-	Set(key, value []byte) error
-	PSet(keys, values [][]byte) error
-	Get(key []byte) ([]byte, bool, error)
-	PGet(keys [][]byte) ([][]byte, []bool, error)
-	Del(key []byte) (bool, error)
-	Keys(pattern []byte, limit int, withvalues bool) ([][]byte, [][]byte, error)
-	FlushDB() error
-}
-
-func bcopy(b []byte) []byte {
-	r := make([]byte, len(b))
-	copy(r, b)
-	return r
-}
